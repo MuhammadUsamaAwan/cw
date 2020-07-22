e7fd02449f937425f2426e162a9ae510c0cf1756
MuhammadUsamaAwan <underusama@gmail.com>
2020-07-22 05:24:34 -0700

    Updates

---
+++ precache-manifest.f1a1309856286136490f5359635f238e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "644a3a7e842aa90aed73ee876a48585e",
+    "url": "/cw/index.html"
+  },
+  {
+    "revision": "a2d5e2b24d7a1d0ae0a7",
+    "url": "/cw/static/css/main.4fc3fc73.chunk.css"
+  },
+  {
+    "revision": "83b83c180483cd5e5e32",
+    "url": "/cw/static/js/2.d816f321.chunk.js"
+  },
+  {
+    "revision": "d0d6b5033fd77eb6038046252d67d3b7",
+    "url": "/cw/static/js/2.d816f321.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2d5e2b24d7a1d0ae0a7",
+    "url": "/cw/static/js/main.af60b839.chunk.js"
+  },
+  {
+    "revision": "5f423f7470817f1ce965",
+    "url": "/cw/static/js/runtime-main.fb35fb2a.js"
+  },
+  {
+    "revision": "aeae6221c9e8a8dcbd35021aa4866a66",
+    "url": "/cw/static/media/discount_banner.aeae6221.png"
+  },
+  {
+    "revision": "a86266891b40c271b4ebdb476afba12e",
+    "url": "/cw/static/media/logo.a8626689.svg"
+  },
+  {
+    "revision": "57e9be2b623da05dcfff24fe42014029",
+    "url": "/cw/static/media/order_banner.57e9be2b.jpg"
+  },
+  {
+    "revision": "0917a615a43d56f1ff0c51430915d242",
+    "url": "/cw/static/media/premium.0917a615.svg"
+  },
+  {
+    "revision": "a4ba4473b2842889fccdb81e9fb57d70",
+    "url": "/cw/static/media/services_banner.a4ba4473.jpg"
+  },
+  {
+    "revision": "6ee3cf562e13e05cdf11cc0bb792ff1d",
+    "url": "/cw/static/media/user1.6ee3cf56.jpg"
+  },
+  {
+    "revision": "ef58ef1c1b2ff9dbb2b0a44e3103fc9c",
+    "url": "/cw/static/media/why_banner.ef58ef1c.png"
+  },
+  {
+    "revision": "1505863ea27d28ee4d80815d079307c3",
+    "url": "/cw/static/media/zero-plag.1505863e.svg"
+  }
+]);
